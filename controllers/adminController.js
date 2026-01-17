6ed0beeaec84045ba79c47167fa3fb971bf40123
anime024 <animeshraj1618@gmail.com>
2026-01-17 15:32:01 +0530

    fronted fixed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Order = require("../models/Order");
+const excelJS=require("exceljs");
 
 async function handleadminDashboard(req, res) {
   try {
@@ -32,4 +33,110 @@ async function handleadminDashboard(req, res) {
   }
 }
 
-module.exports = { handleadminDashboard };
+async function handleviewallregistrations(req, res) {
+  try {
+    const { name, fromDate, toDate } = req.query;
+    let filter = {};
+    if (name) {
+      filter.name = { $regex: name, $options: "i" };
+    }
+    if (fromDate || toDate) {
+      filter.createdAt = {};
+      if (fromDate) filter.createdAt.$gte = new Date(fromDate);
+      if (toDate) filter.createdAt.$lte = new Date(toDate);
+    }
+    const users = await User.find(filter).sort({ createdAt: -1 });
+
+    res.render("viewAllRegistrations", {
+      users,
+      name,
+      fromDate,
+      toDate
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+}
+
+async function handleviewsuccessfulldonations(req, res){
+  try {
+    const paidOrders = await Order.find({ status: "paid" })
+      .populate("userEmail", "email name") // get email & name
+      .sort({ createdAt: -1 });
+
+    res.render("successfulDonations", { orders: paidOrders });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+}
+
+async function handleviewalldonations  (req, res)  {
+  try {
+    // Fetch all orders, populate user email
+    const allOrders = await Order.find({})
+      .populate("userEmail", "email name") // get email & name
+      .sort({ createdAt: -1 }); // latest first
+
+      const donationSum = await Order.aggregate([
+      { $match: { status: "paid" } },
+      {
+        $group: {
+          _id: null,
+          totalAmount: { $sum: "$amount" },
+        },
+      },
+    ]);
+
+    const totalDonationAmount = donationSum[0]?.totalAmount || 0;
+
+    res.render("allDonations", { orders: allOrders,totalDonationAmount: totalDonationAmount / 100, });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+}
+
+//EXPORT USER
+async function handleexportUser(req,res){
+try{
+  const workbook=new excelJS.Workbook();
+  const worksheet=workbook.addWorksheet("My Users");
+  worksheet.columns=[
+    {header:"S.No",key:"s_no"},
+    {header:"Name",key:"name"},
+    {header:"Email",key:"email"},
+    {header:"Role",key:"role"},
+    {header:"Donations",key:"donations"},
+    
+  ];
+
+  let counter=1;
+
+  const userData=await User.find({});
+   userData.forEach((user)=>{
+    user.s_no=counter;
+    worksheet.addRow(user);
+    counter++;
+  })
+
+  worksheet.getRow(1).eachCell((cell)=>{
+    cell.font={bold:true};
+  })
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheatml.sheet"
+  );
+
+  res.setHeader("Content-Disposition",`attachment;filename=users.xlsx`);
+
+  return workbook.xlsx.write(res).then(()=>{
+    res.status(200);
+  })
+}catch(error){
+  console.log("ERROR DURING EXPORTING ",error.message)
+}
+}
+module.exports = { handleadminDashboard,handleviewallregistrations,handleviewsuccessfulldonations,handleviewalldonations,handleexportUser};
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { checkForAuhentication, requireadmin } = require("../middleware/auth");
 const adminRouter = express.Router();
-const { handleadminDashboard } = require("../controllers/adminController");
+const {
+  handleadminDashboard,
+  handleviewallregistrations,handleviewsuccessfulldonations,handleviewalldonations,handleexportUser
+} = require("../controllers/adminController");
 const User = require("../models/user");
 const Order = require("../models/Order");
 
@@ -9,45 +12,12 @@ adminRouter.get(
   "/dashboard",
   checkForAuhentication,
   requireadmin,
-  handleadminDashboard,
+  handleadminDashboard
 );
-adminRouter.get("/viewallregistrations", async (req, res) => {
-  try {
-    const users = await User.find().sort({ createdAt: -1 });
+adminRouter.get("/viewallregistrations", handleviewallregistrations);
 
-    res.render("viewAllRegistrations", {
-      users,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-});
+adminRouter.get("/viewsuccessfulldonations", handleviewsuccessfulldonations);
 
-adminRouter.get("/viewsuccessfulldonations", async (req, res) => {
-  try {
-    const paidOrders = await Order.find({ status: "paid" })
-      .populate("userEmail", "email name") // get email & name
-      .sort({ createdAt: -1 });
-
-    res.render("successfulDonations", { orders: paidOrders });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server Error");
-  }
-});
-
-adminRouter.get("/viewalldonations", async (req, res) => {
-  try {
-    // Fetch all orders, populate user email
-    const allOrders = await Order.find({})
-      .populate("userEmail", "email name") // get email & name
-      .sort({ createdAt: -1 }); // latest first
-
-    res.render("allDonations", { orders: allOrders });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server Error");
-  }
-});
+adminRouter.get("/viewalldonations", handleviewalldonations);
+adminRouter.get("/exportUser", handleexportUser);
 module.exports = adminRouter;
